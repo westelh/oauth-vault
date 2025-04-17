3c7ffd6ab4aeb1cf9ea91635705b5a31a0be28cb
Taisei Nishiumi <westelh@gmail.com>
2025-04-18 01:50:42 +0900

    Login.ktの簡潔化ˆ

--- src/main/kotlin/Login.kt
+++ src/main/kotlin/Login.kt
@@ -3,7 +3,6 @@ package dev.westelh
 import dev.westelh.vault.Vault
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
-import io.ktor.client.plugins.logging.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
@@ -29,26 +28,25 @@ fun Application.configureSecurity() {
             }
         }
 
-        val google = OAuthServerSettings.OAuth2ServerSettings(
-            name = "google",
-            authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
-            accessTokenUrl = "https://accounts.google.com/o/oauth2/token",
-            requestMethod = HttpMethod.Post,
-            clientId = this@configureSecurity.environment.config.property("google.oauth.clientId").getString(),
-            clientSecret = this@configureSecurity.environment.config.property("google.oauth.clientSecret").getString(),
-            defaultScopes = listOf(
-                "https://www.googleapis.com/auth/userinfo.profile",
-                "https://www.googleapis.com/auth/userinfo.email",
-                "https://mail.google.com/",
-            ),
-            extraAuthParameters = listOf("access_type" to "offline")
-        )
         oauth("auth-oauth-google") {
-            urlProvider = { this@configureSecurity.environment.config.property("google.oauth.callback").getString() }
-            providerLookup = { google }
-            client = HttpClient(Apache) {
-                install(Logging) {
-                    level = LogLevel.INFO
+            with(env.config.config("google.oauth")) {
+                urlProvider = { property("callback").getString() }
+                client = HttpClient(Apache)
+                providerLookup = {
+                    OAuthServerSettings.OAuth2ServerSettings(
+                        name = "google",
+                        authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
+                        accessTokenUrl = "https://accounts.google.com/o/oauth2/token",
+                        requestMethod = HttpMethod.Post,
+                        clientId = property("clientId").getString(),
+                        clientSecret = property("clientSecret").getString(),
+                        defaultScopes = listOf(
+                            "https://www.googleapis.com/auth/userinfo.profile",
+                            "https://www.googleapis.com/auth/userinfo.email",
+                            "https://mail.google.com/",
+                        ),
+                        extraAuthParameters = listOf("access_type" to "offline")
+                    )
                 }
             }
         }
