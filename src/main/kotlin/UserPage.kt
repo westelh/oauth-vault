dee43752c99634788358dffaee3194bc025c48a8
Taisei Nishiumi <westelh@gmail.com>
2025-04-28 14:34:25 +0900

    ユーザーページを追加

--- src/main/kotlin/UserPage.kt
+++ src/main/kotlin/UserPage.kt
@@ -1,14 +1,22 @@
 package dev.westelh
 
+import dev.westelh.model.OAuthCodes
+import dev.westelh.model.expiresAt
 import io.ktor.server.application.Application
 import io.ktor.server.auth.OAuthAccessTokenResponse
 import io.ktor.server.auth.authenticate
 import io.ktor.server.auth.authentication
+import io.ktor.server.response.respond
 import io.ktor.server.response.respondRedirect
+import io.ktor.server.routing.RoutingContext
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
+import io.ktor.server.sessions.SessionStorageMemory
 import io.ktor.server.sessions.Sessions
+import io.ktor.server.sessions.cookie
+import io.ktor.server.sessions.get
+import io.ktor.server.sessions.sessions
 
 // User page module
 
@@ -18,18 +26,37 @@ fun Application.configureUserPage() {
 
     routing {
         route("/user") {
-            install(Sessions)
+            install(Sessions) {
+                cookie<OAuthCodes>("oauth_principal", SessionStorageMemory()) {
+                }
+            }
 
             authenticate("auth-oauth-vault") {
                 get("/oidc/login") {
-                    call.respondRedirect("/oidc/user/callback")
+                    call.respondRedirect("/oidc/callback")
                 }
 
                 get("/oidc/callback") {
-                    val principal: OAuthAccessTokenResponse.OAuth2 = call.authentication.principal()!!
+                    val principal: OAuthAccessTokenResponse.OAuth2= call.authentication.principal()!!
+                    call.sessions.set("oauth_principal", OAuthCodes(principal))
                     call.respondRedirect("/")
                 }
             }
+
+            get("/summary") {
+                ensureUserSession { principal ->
+                    call.respond("Your session is valid. Expires at ${principal.expiresAt()}.")
+                }
+            }
         }
+    }
+}
+
+private suspend fun RoutingContext.ensureUserSession(block: suspend (OAuthCodes) -> Unit) {
+    val principal = call.sessions.get<OAuthCodes>()
+    if (principal == null) {
+        call.respondRedirect("/user/oidc/login")
+    } else {
+        block(principal)
     }
 }
